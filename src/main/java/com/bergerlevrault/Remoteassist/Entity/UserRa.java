7e290d6a018a5531612ed8b81fd029aa0f002dec
othmane tozy <tozyothmane123@gmail.com>
2024-05-27 17:19:30 +0100

    update docker

--- src/main/java/com/bergerlevrault/Remoteassist/Entity/UserRa.java
+++ src/main/java/com/bergerlevrault/Remoteassist/Entity/UserRa.java
@@ -26,4 +26,59 @@ public class UserRa {
     @OneToMany(mappedBy = "user")
     private Set<SessionRa> sessions;
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Set<SessionRa> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(Set<SessionRa> sessions) {
+        this.sessions = sessions;
+    }
 }
